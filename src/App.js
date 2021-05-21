9f0d7f0e84d10a4fc4106ed0308719a5e67ce741
Fabricio Lima <fabricio_lima_carvalho@hotmail.com>
2021-05-20 22:33:11 -0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { useHistory, Route } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Notas from './Notas';
+
+import AppContext from './AppContext';
+
+import useDidMount from './mountControlls';
+
+function App ()
+{
+    let history = useHistory();
+
+    const [nome, setNome] = useState('');
+    const [senha, setSenha] = useState('');
+    const [autenticado, setAutenticado] = useState(false);
+    const [loginInvalido, setLoginInvalido] = useState(false);
+
+    useDidMount(() =>
+    {
+        if (autenticado)
+        {
+            history.push('/notas');
+        }
+        else
+        {
+            history.push('/login');
+        }
+    });
+
+    function userAutenticado ()
+    {
+        setAutenticado(true, history.push('/notas'));
+    }
+
+    function logout ()
+    {
+        setNome('');
+        setSenha('');
+        setAutenticado(false, history.push('/login'));
+    }
+
+    const validarAcesso = function ()
+    {
+        if (nome === 'admin' && senha === 'admin')
+        {
+            userAutenticado();
+        }
+        else
+        {
+            setLoginInvalido(true);
+        }
+    }
+
+    return <>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous" />
+        <AppContext.Provider value={ {
+            autenticado, userAutenticado, logout
+        } }>
+            <Route exact path="/login" render={ () => <div>
+                <div className="container">
+                    <h1>Controle de acesso às notas pessoais</h1>
+                    { loginInvalido && (<p style={ { color: "red" } }> Usuário e/ou senha inválido(s)! Tente novamente</p>) }
+                    <label>Nome:</label>
+                    <input className="form-control my-2" type="text" value={ nome } onChange={ (e) => setNome(e.target.value) } />
+                    <label>Senha:</label>
+                    <input className="form-control my-2" type="password" value={ senha } onChange={ (e) => setSenha(e.target.value) } />
+                    <button className="btn btn-primary my-2 w-100" onClick={ validarAcesso }>Acessar</button>
+                </div>
+            </div>
+            } />
+            <Route exact path="/notas" render={ () => <div>
+                <Notas />
+            </div>
+            } />
+        </AppContext.Provider></>;
 }
 
 export default App;
---
+++ src/Notas.js
@@ -0,0 +1,212 @@
+import React, { useState, useContext } from 'react';
+
+import AppContext from './AppContext';
+
+import useDidMount from './mountControlls';
+
+function Notas ()
+{
+    let { autenticado, logout } = useContext(AppContext);
+
+    const [filtro, setFiltro] = useState('');
+    const [notas, setNotas] = useState([]);
+    const [novaNota, setNovaNota] = useState(false);
+    const [selectNota, setSelectNota] = useState(false);
+    const [auth] = useState(autenticado);
+
+    const [novoTitulo, setNovoTitulo] = useState('');
+    const [novaCategoria, setNovaCategoria] = useState('');
+    const [novoConteudo, setNovoConteudo] = useState('');
+    const [novaData] = useState(Date());
+
+    const [mostraId, setMostraId] = useState(0);
+    const [mostraTitulo, setMostraTitulo] = useState('');
+    const [mostraCategoria, setMostraCategoria] = useState('');
+    const [mostraConteudo, setMostraConteudo] = useState('');
+
+    //CRUD
+    async function createNotas (nota)
+    {
+        const result = await fetch('http://localhost:4000/notas', {
+            method: 'post',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                ...nota
+            })
+        });
+
+        console.log('Create result: ', result);
+
+        await readNotas();
+
+        setNovaNota(false);
+        setSelectNota(false);
+    }
+    async function readNotas ()
+    {
+        const result = await fetch('http://localhost:4000/notas');
+        const json = await result.json();
+
+        setNotas(json);
+    }
+    async function readNota (id)
+    {
+        const result = await fetch(`http://localhost:4000/notas/${ id }`);
+        const json = await result.json();
+
+        setMostraId(json.id);
+        setMostraTitulo(json.titulo);
+        setMostraCategoria(json.categoria);
+        setMostraConteudo(json.conteudo);
+
+        await readNotas();
+    }
+    async function updateNotas (nota)
+    {
+        const result = await fetch(`http://localhost:4000/notas/${ nota.id }`, {
+            method: 'put',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                ...nota
+            })
+        });
+
+        console.log('Updade result: ', result);
+
+        await readNotas();
+
+        setNovaNota(false);
+        setSelectNota(false);
+    }
+    async function deleteNotas (id)
+    {
+        const result = await fetch(`http://localhost:4000/notas/${ id }`, {
+            method: 'delete',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+
+        console.log('Delete result: ', result);
+
+        await readNotas();
+
+        setNovaNota(false);
+        setSelectNota(false);
+    }
+
+    useDidMount(() =>
+    {
+        if (!auth)
+        {
+            logout();
+        }
+        else
+        {
+            readNotas();
+        }
+    });
+
+    function mostrarNota (id)
+    {
+        readNota(id);
+
+        setNovaNota(false);
+        setSelectNota(true);
+    }
+
+    function mostrarCadastroNota ()
+    {
+        setNovaNota(true);
+        setSelectNota(false);
+    }
+
+    const notasFiltradas = notas.filter(item => item.titulo.toLowerCase().indexOf(filtro.toLowerCase()) >= 0);
+
+    return (<>
+        <div className="container my-5">
+            <div className="row">
+                <div className="col-auto">
+                    <input className="form-control mb-4" placeholder="Pesquisar título" type="text" value={ filtro } onChange={ (e) => setFiltro(e.target.value) } />
+                    <div className="row">
+                        <div className="col"><h2>Todas as notas</h2></div>
+                        <div className="col-auto"><button className="btn btn-primary" onClick={ (e) =>
+                        {
+                            mostrarCadastroNota();
+                        } }>+</button></div>
+                    </div>
+                    <ul className="list-group my-3">
+                        { notasFiltradas.map(nota => <li className="list-group-item" onClick={ (e) => { mostrarNota(nota.id) } }>{ nota.titulo }</li>) }
+                    </ul>
+                    <div>
+                        <button className="btn btn-primary w-100" onClick={ logout }>Sair</button>
+                    </div>
+                </div>
+
+                <div className="col">
+                    { novaNota ? <>
+                        <div>
+                            <div>
+                                <h2>Cadastro de notas</h2>
+                            </div>
+                            <div class="mb-3">
+                                <label htmlFor="titulo" className="form-label">Título</label>
+                                <input type="text" className="form-control" id="titulo" value={ novoTitulo } onChange={ (e) => setNovoTitulo(e.target.value) } />
+                            </div>
+                            <div class="mb-3">
+                                <label htmlFor="categoria" className="form-label">Categoria</label>
+                                <input type="text" className="form-control" id="categoria" value={ novaCategoria } onChange={ (e) => setNovaCategoria(e.target.value) } />
+                            </div>
+                            <div class="mb-3">
+                                <label htmlFor="conteudo" className="form-label">Conteudo</label>
+                                <textarea type="text" className="form-control" id="conteudo" value={ novoConteudo } onChange={ (e) => setNovoConteudo(e.target.value) } />
+                            </div>
+                            <div className="text-end">
+                                <button className="btn btn-primary" onClick={ (e) => createNotas(Object.assign({}, { titulo: novoTitulo, categoria: novaCategoria, conteudo: novoConteudo, data: novaData })) } > Cadastrar nota</button>
+                            </div>
+                        </div>
+                    </> : "" }
+                    { selectNota ? <>
+                        <div>
+                            <div className="row">
+                                <div className="col">
+                                    <h2>Alteração de notas</h2>
+                                </div>
+                                <div className="col text-end">
+                                    <button className="btn btn-danger" onClick={ (e) => deleteNotas(mostraId) }>Apagar nota</button>
+                                </div>
+                            </div>
+                            <div class="mb-3">
+                                <label htmlFor="titulo" className="form-label">Título</label>
+                                <input type="text" className="form-control" id="titulo" value={ mostraTitulo } onChange={ (e) => setMostraTitulo(e.target.value) } />
+                            </div>
+                            <div class="mb-3">
+                                <label htmlFor="categoria" className="form-label">Categoria</label>
+                                <input type="text" className="form-control" id="categoria" value={ mostraCategoria } onChange={ (e) => setMostraCategoria(e.target.value) } />
+                            </div>
+                            <div class="mb-3">
+                                <label htmlFor="conteudo" className="form-label">Conteudo</label>
+                                <textarea type="text" className="form-control" id="conteudo" value={ mostraConteudo } onChange={ (e) => setMostraConteudo(e.target.value) } />
+                            </div>
+                            <div className="text-end">
+                                <button className="btn btn-primary" onClick={ (e) => updateNotas(Object.assign({}, { id: mostraId, titulo: mostraTitulo, categoria: mostraCategoria, conteudo: mostraConteudo, data: novaData })) } > Alterar nota</button>
+                            </div>
+                        </div>
+                    </> : "" }
+                    { !novaNota && !selectNota ? <>
+                        <div>
+                            <h2>Bem vindo</h2>
+                        </div>
+                    </> : "" }
+                </div>
+            </div>
+        </div>
+    </>
+    );
+}
+
+export default Notas;
