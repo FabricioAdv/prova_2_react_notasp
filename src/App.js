8bd30bb138b13b23096dd8adc5c2cc34f6769ec1
Fabricio Lima <fabricio_lima_carvalho@hotmail.com>
2021-05-27 19:07:31 -0300

    Controle de longPress nos itens da lista e ajustes

--- src/App.js
+++ src/App.js
@@ -53,7 +53,7 @@ function App ()
     }
 
     return <>
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous" />
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossOrigin="anonymous" />
         <AppContext.Provider value={ {
             autenticado, userAutenticado, logout
         } }>
--- src/Notas.js
+++ src/Notas.js
@@ -8,10 +8,13 @@ function Notas ()
 {
     let { autenticado, logout } = useContext(AppContext);
 
+    var holdNpress;
+
     const [filtro, setFiltro] = useState('');
     const [notas, setNotas] = useState([]);
     const [novaNota, setNovaNota] = useState(false);
     const [selectNota, setSelectNota] = useState(false);
+    const [holdNota, setHoldNota] = useState(false);
     const [auth] = useState(autenticado);
 
     const [novoTitulo, setNovoTitulo] = useState('');
@@ -24,6 +27,9 @@ function Notas ()
     const [mostraCategoria, setMostraCategoria] = useState('');
     const [mostraConteudo, setMostraConteudo] = useState('');
     const [mostraData, setMostraData] = useState('');
+
+    const [holdId, setHoldId] = useState(0);
+    const [holdTitulo, setHoldTitulo] = useState('');
 
     //CRUD
     async function createNotas (nota)
@@ -99,6 +105,7 @@ function Notas ()
 
         setNovaNota(false);
         setSelectNota(false);
+        setHoldNota(false);
     }
 
     useDidMount(() =>
@@ -125,25 +132,97 @@ function Notas ()
     {
         setNovaNota(true);
         setSelectNota(false);
+        setHoldNota(false);
+
+        setMostraId(0);
+    }
+
+    // Controle de click de um item nam lista
+    function mouseDown (id, titulo)
+    {
+        holdNpress = setTimeout(mostraHold, 500, id, titulo);
+    }
+
+    function mouseUp (id)
+    {
+        if (!holdNota)
+        {
+            mostrarNota(id);
+        }
+
+        clearTimeout(holdNpress);
+    }
+
+    function mostraHold (id, titulo)
+    {
+        setHoldId(id, setHoldTitulo(titulo, setHoldNota(true)));
     }
 
     const notasFiltradas = notas.filter(item => item.titulo.toLowerCase().indexOf(filtro.toLowerCase()) >= 0);
+
+    function menuStyle (id)
+    {
+        var menStyle = 'list-group-item list-group-item-action';
+
+        if (id === mostraId)
+        {
+            menStyle += ' active';
+        }
+
+        if (holdNota)
+        {
+            menStyle += ' disabled';
+        }
+
+        return menStyle
+    }
 
     return (<>
         <div className="container my-5">
             <div className="row">
                 <div className="col-auto">
                     <input className="form-control mb-4" placeholder="Pesquisar título" type="text" value={ filtro } onChange={ (e) => setFiltro(e.target.value) } />
-                    <div className="row">
+                    <div className="row mb-3">
                         <div className="col"><h2>Todas as notas</h2></div>
                         <div className="col-auto"><button className="btn btn-primary" onClick={ (e) =>
                         {
                             mostrarCadastroNota();
                         } }>+</button></div>
                     </div>
-                    <ul className="list-group my-3">
-                        { notasFiltradas.map(nota => <li key={ nota.id } className="list-group-item" onClick={ (e) => { mostrarNota(nota.id) } }>{ nota.titulo }</li>) }
-                    </ul>
+                    <div className="list-group mb-3">
+                        { notasFiltradas.map(nota => <button type="button" key={ nota.id } className={ menuStyle(nota.id) } onMouseDown={ (e) => { mouseDown(nota.id, nota.titulo) } } onMouseUp={ (e) => { mouseUp(nota.id) } }>{ nota.titulo }</button>) }
+                    </div>
+                    <div className="mb-3">
+                        <div className="px-3 py-1 border-bottom border-3">
+                            <span>Ações</span>
+                        </div>
+                        <div className="row px-3 pt-1">
+                            <div className="col-7 border-bottom">Clicar</div>
+                            <div className="col border-bottom">Abrir</div>
+                        </div>
+                        <div className="row px-3">
+                            <div className="col-7">Clicar e segurar</div>
+                            <div className="col">Opções</div>
+                        </div>
+                    </div>
+                    { holdNota ? <>
+                        <div className="border rounded-2 mb-3">
+                            <div className="mx-3 my-2">
+                                <span>Nota selecionada: { holdTitulo }</span>
+                            </div>
+                            <div className="mx-3 my-2">
+                                <span>Apagar a nota selecionada?</span>
+                            </div>
+                            <div className="row mx-3 my-2">
+                                <div className="col">
+                                    <button className="btn btn-primary w-100" onClick={ (e) => { setHoldNota(false) } }>Cancelar</button>
+                                </div>
+                                <div className="col">
+                                    <button className="btn btn-danger w-100" onClick={ (e) => deleteNotas(holdId) }>Apagar</button>
+                                </div>
+                            </div>
+                        </div>
+                    </> : "" }
                     <div>
                         <button className="btn btn-primary w-100" onClick={ logout }>Sair</button>
                     </div>
